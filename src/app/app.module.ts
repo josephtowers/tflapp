e1ab0a7b13ba2186d3f13963f5858d0c67b9d710
Joseph Towers <josse.1397@gmail.com>
2018-08-09 17:36:44 -0400

    Added components for each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListComponent } from './list/list.component';
+import { AboutComponent } from './about/about.component';
+import { SkinComponent } from './skin/skin.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'skin', component: SkinComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'list', component: ListComponent },  
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    PageNotFoundComponent,
+    ListComponent,
+    AboutComponent,
+    SkinComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
