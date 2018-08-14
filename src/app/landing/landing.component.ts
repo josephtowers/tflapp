e2e89762ceca96d5cb002d3a4d800a54537891b8
Joseph Towers <josse.1397@gmail.com>
2018-08-14 19:36:26 -0400

    Fixed animations running before expected

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TweenMax } from 'gsap';
-import { TimelineMax } from 'gsap';
+import { TimelineMax, Circ } from 'gsap';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-landing',
@@ -19,12 +19,14 @@ export class LandingComponent implements OnInit {
     $(document).ready(function () {
       const t1 = new TimelineMax();
       let isScrolling = false;
-      t1.from('.firstone', 1, { opacity: 0, x: -500, delay: 1 });
+      t1.from('.firstone', 1, { opacity: 0, x: '-1000vw', ease: Circ.easeOut });
+      TweenMax.from('img', 1, { opacity: 0, x: '1000vw', ease: Circ.easeOut });
       $('body').on('mousewheel', function (e) {
         if ((<any>e).originalEvent.wheelDelta <= 0 && !isScrolling) {
           isScrolling = true;
-          TweenMax.to('.landing-container', 1.5, {
+          TweenMax.to('.landing-container', 1, {
             y: '-100%',
+            ease: Circ.easeInOut,
             onComplete: () => {
               console.log('happening');
               $('a.please')[0].click()
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { TweenMax } from 'gsap';
+import { TweenMax, Circ } from 'gsap';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-list',
@@ -18,16 +18,17 @@ export class ListComponent implements OnInit {
   constructor(public router: Router) {
     router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        TweenMax.staggerFrom('.list-item', 2, { opacity: 0, y: '200vh'}, 0.2);
-        TweenMax.staggerFrom('.featured-item', 2, {opacity: 0, x: '200vw'}, 0.2);
+        TweenMax.from('.input-group, .text-container', 1, { opacity: 0, x: '-1000vw',  ease: Circ.easeOut});
+        TweenMax.staggerFrom('.list-item', 1, { opacity: 0, x: '-1000vw',  ease: Circ.easeOut}, 0.2);
+        TweenMax.staggerFrom('.featured-item', 2, {opacity: 0, x: '1000vw',  ease: Circ.easeOut}, 0.2);
         $(document).ready(function() {
           $('.list-item').click(function(){
             $('html, body').css({
               overflow: 'hidden',
               height: '100%'
           });
-            TweenMax.to('.first-column ', 1, { opacity: 0, x: '-200vw'});
-            TweenMax.to('.featured-item', 1, {opacity: 0, x: '200vw',
+            TweenMax.to('.first-column ', 1, { opacity: 0, x: '-1000vw', ease: Circ.easeIn});
+            TweenMax.to('.featured-item', 1, {opacity: 0, x: '1000vw', ease: Circ.easeIn,
             onComplete: () => {
               $('a.getskin')[0].click()
              }});
